0ddd2884ad397804fcff17fc2b8b334949f84c9e
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-02-13 16:56:57 +0100

    Add wrapper to support custom templates

--- functions.php
+++ functions.php
@@ -1,3 +1,40 @@
 <?php
 
 header('X-Frame-Options GOFORIT');
+
+function jf_template_path() {
+  return Wrapper::$main_template;
+}
+
+function jf_template_base() {
+  return Wrapper::$base;
+}
+
+class Wrapper {
+  /**
+   * Stores the full path to the main template file
+   */
+  static $main_template;
+
+  /**
+   * Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+   */
+  static $base;
+
+  static function jf_wrap( $template ) {
+    self::$main_template = $template;
+
+    self::$base = substr( basename( self::$main_template ), 0, -4 );
+
+    if ( 'index' == self::$base )
+      self::$base = false;
+
+    $templates = array( 'wrapper.php' );
+
+    if ( self::$base )
+      array_unshift( $templates, sprintf( 'wrapper-%s.php', self::$base ) );
+
+    return locate_template( $templates );
+  }
+}
+add_filter( 'template_include', array( 'Wrapper', 'jf_wrap' ), 99 );
---
+++ wrapper.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9 ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]><html class="no-js lt-ie9 ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes() ?>> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--
+
+      Developed by Hoppinger BV 2014 - 2015
+      http://www.hoppinger.com
+
+    -->
+
+  <?php wp_head(); ?>
+
+</head>
+
+  <body <?php body_class(); ?>>
+
+  <div id="body-classes" <?php body_class(); ?>></div>
+
+  <?php include jf_template_path(); ?>
+
+  <?php wp_footer(); ?>
+
+</html>
